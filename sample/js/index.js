99b60808d12f94b70fd1a33419fe66a2a45d8589
joji-sg <j-suganuma1276@st.kyoto-art.ac.jp>
2020-11-10 14:34:07 +0900

    vroidref

--- sample/js/index.js
+++ sample/js/index.js
@@ -1,113 +1,94 @@
-window.addEventListener("DOMContentLoaded", init);
-
+window.addEventListener('DOMContentLoaded', init);
 function init() {
-  // レンダラーを作成
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#canvas")
-  });
-  // ウィンドウサイズ設定
-  width = document.getElementById("main_canvas").getBoundingClientRect().width;
-  height = document.getElementById("main_canvas").getBoundingClientRect()
-    .height;
-  renderer.setPixelRatio(1);
-  renderer.setSize(width, height);
-  console.log(window.devicePixelRatio);
-  console.log(width + ", " + height);
+    // レンダラーを作成
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#canvas')
+    });
+    // ウィンドウサイズ設定
+    width = document.getElementById('main_canvas').getBoundingClientRect().width;
+    height = document.getElementById('main_canvas').getBoundingClientRect().height;
+    renderer.setPixelRatio(1);
+    renderer.setSize(width, height);
+    console.log(window.devicePixelRatio);
+    console.log(width+", "+height);
+ 
+    // シーンを作成
+    const scene = new THREE.Scene();
+ 
+    // カメラを作成
+    camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+    camera.position.set(0, 400, -1000);
+    
+    const controls = new THREE.OrbitControls(camera);
+    //camera.lookAt(new THREE.Vector3(0, 400, 0));
 
-  // シーンを作成
-  const scene = new THREE.Scene();
+    // Load GLTF or GLB
+    const loader = new THREE.GLTFLoader();
+    const url = 'http://localhost/Three.js_sample/VRoid.glb';
+    
+    let model = null;
+    loader.load(
+        url, 
+        function ( gltf ){
+            model = gltf.scene;
+            model.name = "model_with_cloth";
+            model.scale.set(400.0, 400.0, 400.0);
+            model.position.set(0,-400,0);
+            scene.add( gltf.scene );
 
-  // カメラを作成
-  camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-  camera.position.set(0, 400, -1000);
+            model["test"] = 100;
+            console.log("model");
+        },
+        function ( error ) {
+            console.log( 'An error happened' );
+            console.log( error );
+        }
+    );
+    renderer.gammaOutput = true;
+    renderer.gammaFactor = 2.2;
 
-  const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-  // Load GLTF or GLB
-  const loader = new THREE.GLTFLoader();
-  const tjsmountUrl = "https://kuad-xtechdesign.github.io/js-various-joji-sg/tjsmount.glb";
+    // 平行光源
+    const light = new THREE.DirectionalLight(0xFFFFFF);
+    light.intensity = 2; // 光の強さを倍に
+    light.position.set(1, 1, 1);
+    // シーンに追加
+    scene.add(light);
 
-  loader.load(
-    tjsmountUrl,
-    function(gltf) {
-      const model = gltf.scene;
-      model.scale.set(400.0, 400.0, 400.0);
-      model.position.set(0, -400, 0);
-
-      // 外壁を半透明にする
-      cube = gltf.scene.children[4];
-      cube.children.forEach(child => {
-        child.material.opacity = 0.2;
-        child.material.transparent = true;
-        child.material.side = THREE.DoubleSide;
-      });
-
-      scene.add(gltf.scene);
-    },
-    function(error) {
-      console.log("An error happened");
-      console.log(error);
+    // 初回実行
+    tick();
+    function tick() {
+        controls.update();
+        
+        scene.traverse(function(obj) {
+            if(obj.name == "J_Bip_C_Chest"){
+                obj.rotation.z += 2 /180*3.1415;
+            }
+        });
+        if (model != null){
+            console.log(model);
+        }
+        renderer.render(scene, camera);
+        requestAnimationFrame(tick);
     }
-  );
+    
 
-  const droneUrl = "https://kuad-xtechdesign.github.io/js-various-joji-sg/tjsmount.glb";
-  let drone;
-  // ドローンロード
-  loader.load(
-    droneUrl,
-    function(gltf) {
-      drone = gltf.scene;
-      // model.name = "model_with_cloth";
-      drone.scale.set(20.0, 20.0, 20.0);
-      drone.position.set(0, -400, 0);
+    // 初期化のために実行
+    onResize();
+    // リサイズイベント発生時に実行
+    window.addEventListener('resize', onResize);
+    function onResize() {
+        // サイズを取得
+        width = document.getElementById('main_canvas').getBoundingClientRect().width;
+        height = document.getElementById('main_canvas').getBoundingClientRect().height;
 
-      console.log(drone);
-      scene.add(gltf.scene);
-    },
-    function(error) {
-      console.log("An error happened");
-      console.log(error);
+        // レンダラーのサイズを調整する
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(width, height);
+
+        // カメラのアスペクト比を正す
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        console.log(width);
     }
-  );
-  renderer.gammaOutput = true;
-  renderer.gammaFactor = 2.2;
-
-  // 平行光源
-  const light = new THREE.DirectionalLight(0xffffff);
-  light.intensity = 2; // 光の強さを倍に
-  light.position.set(1, 1, 1);
-  // シーンに追加
-
-  scene.add(light);
-
-  // SSEから情報を受け取る
-  const evtSource = new EventSource(
-    "https://webflux-sse-demo.herokuapp.com/stream-sse",
-    { withCredentials: false }
-  );
-  let sseData;
-  evtSource.addEventListener("periodic-event", function(e) {
-    sseData = JSON.parse(e.data);
-  });
-
-  // 初回実行
-  tick();
-
-  function tick() {
-    controls.update();
-
-    if (typeof sseData !== "undefined") {
-      const x = 0;
-      const y = sseData.time.slice(-2) * 10;
-      const z = 0;
-      // const x = sseData.time.slice(-2) * 1;
-      // const y = sseData.time.slice(-2) * 2;
-      // const z = sseData.time.slice(-2) * 10;
-      drone.position.set(x, y, z);
-      console.log("x:%d, y:%d, z:%d", x, y, z);
-    }
-
-    renderer.render(scene, camera);
-    requestAnimationFrame(tick);
-  }
 }
